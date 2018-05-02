3d2816861c01a73dcb882329109c91f4669d3004
weisshaarb <weisshaarb@10.134.130.155>
2018-05-02 15:16:15 +0200

    asdasd

--- Portfolio2.0/js/main.js
+++ Portfolio2.0/js/main.js
@@ -1,41 +1,59 @@
 $(document).ready(function() {
 
-	$("#fig01").hover(function() {
-			$("#exp-txt02").hide(300);
-			$("#exp-txt01").toggle(300);
-		});
+	$("#fig01").click(function() {
+		$("#exp01").toggleClass("align-l");
+		$("#exp-txt02").hide(300);
+		$("#exp-txt01").toggle(300);
+	});
 	
-	$("#fig02").hover(function() {
+	$("#fig02").click(function() {
+		$("#exp02").toggleClass("align-l");
 		$("#exp-txt01").hide(300);
 		$("#exp-txt02").toggle(300);
 	});
 	
-	$("#fig11").hover(function() {
+	$("#fig11").click(function() {
+		$("#proj01").toggleClass("align-l");
 		$("#proj-txt02").hide(300);
 		$("#proj-txt03").hide(300);
 		$("#proj-txt04").hide(300);
 		$("#proj-txt01").toggle(300);
 	});
 	
-	$("#fig12").hover(function() {
+	$("#fig12").click(function() {
+		$("#proj02").toggleClass("align-l");
 		$("#proj-txt01").hide(300);
 		$("#proj-txt03").hide(300);
 		$("#proj-txt04").hide(300);
 		$("#proj-txt02").toggle(300);
 	});
 	
-	$("#fig13").hover(function() {
+	$("#fig13").click(function() {
+		$("#proj03").toggleClass("align-l");
 		$("#proj-txt01").hide(300);
 		$("#proj-txt02").hide(300);
 		$("#proj-txt04").hide(300);
 		$("#proj-txt03").toggle(300);
 	});
 	
-	$("#fig14").hover(function() {
+	$("#fig14").click(function() {
+		$("#proj04").toggleClass("align-l");
 		$("#proj-txt01").hide(300);
 		$("#proj-txt02").hide(300);
 		$("#proj-txt03").hide(300);
 		$("#proj-txt04").toggle(300);
+	});
+	
+	$(".menu").click(function() {
+		$(this).hide(200);
+		$(".menuC").show(200);
+		$(".nav-m").show(200);
+	});
+	
+	$(".menuC").click(function() {
+		$(this).hide(200);
+		$(".menu").show(200);
+		$(".nav-m").hide(200);
 	});
 
 });
