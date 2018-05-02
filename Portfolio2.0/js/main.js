39b2646deb8b69aeb9681870ef30b0d73e6b88e2
weisshaarb <weisshaarb@10.134.130.155>
2018-05-02 11:50:41 +0200

    dsf

--- Portfolio2.0/js/main.js
+++ Portfolio2.0/js/main.js
@@ -1,37 +1,37 @@
 $(document).ready(function() {
 
-	$("#fig01").click(function() {
+	$("#fig01").hover(function() {
 			$("#exp-txt02").hide(300);
 			$("#exp-txt01").toggle(300);
 		});
 	
-	$("#fig02").click(function() {
+	$("#fig02").hover(function() {
 		$("#exp-txt01").hide(300);
 		$("#exp-txt02").toggle(300);
 	});
 	
-	$("#fig11").click(function() {
+	$("#fig11").hover(function() {
 		$("#proj-txt02").hide(300);
 		$("#proj-txt03").hide(300);
 		$("#proj-txt04").hide(300);
 		$("#proj-txt01").toggle(300);
 	});
 	
-	$("#fig12").click(function() {
+	$("#fig12").hover(function() {
 		$("#proj-txt01").hide(300);
 		$("#proj-txt03").hide(300);
 		$("#proj-txt04").hide(300);
 		$("#proj-txt02").toggle(300);
 	});
 	
-	$("#fig13").click(function() {
+	$("#fig13").hover(function() {
 		$("#proj-txt01").hide(300);
 		$("#proj-txt02").hide(300);
 		$("#proj-txt04").hide(300);
 		$("#proj-txt03").toggle(300);
 	});
 	
-	$("#fig14").click(function() {
+	$("#fig14").hover(function() {
 		$("#proj-txt01").hide(300);
 		$("#proj-txt02").hide(300);
 		$("#proj-txt03").hide(300);
